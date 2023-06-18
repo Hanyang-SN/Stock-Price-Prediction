4f2a0589e579c4073a3c53ac84f60b4ca5940298
1092soobin2 <1092soobin2@gmail.com>
2023-06-19 00:30:06 +0900

    train, test data 불러오기

--- prediction/collect_data.py
+++ prediction/collect_data.py
@@ -1,27 +1,62 @@
 import os
 from pykrx import stock
 
-STOCK_DATA_DIR = "./stock_data"
+STOCK_DATA_DIR = "./prediction/stock_data"
+TRAIN_DIR = f"{STOCK_DATA_DIR}/train"
+TEST_DIR = f"{STOCK_DATA_DIR}/test"
+
+
+def make_train_data_file(file_name, ticker):
+    
+    train_file_name = f"{TRAIN_DIR}/{file_name}"
+    
+    # stock 데이터 파일 유무 확인
+    if os.path.isfile(train_file_name):
+        print("File already exists. File name : ", train_file_name)
+        return
+    
+    # 데이터 수집
+    df = stock.get_market_ohlcv("20180101", "20230101", ticker)
+
+    df = df[["시가", "종가", "등락률"]]
+    df.to_csv(train_file_name)
+    print("New file is made. File name : ", train_file_name)
+    
+    
+def make_test_data_file(file_name, ticker):
+    
+    test_file_name = f"{TEST_DIR}/{file_name}"
+    
+    #  stock 데이터 파일 유무 확인
+    if os.path.isfile(test_file_name):
+        print("File already exists. File name : ", test_file_name)
+        return
+
+    # 데이터 수집
+    df = stock.get_market_ohlcv("20230101", "20230601", ticker)
+
+    df = df[["시가", "종가", "등락률"]]
+    df.to_csv(test_file_name)
+    print("New file is made. File name : ", test_file_name)
+
 
 def get_stock_price_data(ticker_str : str):
     
     # stock 데이터 디렉토리 생성
     if not os.path.isdir(STOCK_DATA_DIR):
         os.mkdir(STOCK_DATA_DIR)
-
-    file_name = f"{STOCK_DATA_DIR}/{ticker_str}-{stock.get_market_ticker_name(ticker_str)}.csv"
-        
-    # stock 데이터 파일 유무 확인
-    if os.path.isfile(file_name):
-        print("File already exists.")
-        return
     
-    # 데이터 수집
-    df = stock.get_market_ohlcv("20180101", "20230101", ticker_str)
+    if not os.path.isdir(TRAIN_DIR):
+        os.mkdir(TRAIN_DIR)
+        
+    if not os.path.isdir(TEST_DIR):
+        os.mkdir(TEST_DIR)
+    
+    file_name = f"{ticker_str}-{stock.get_market_ticker_name(ticker_str)}.csv"
+    
+    make_train_data_file(file_name, ticker_str)
+    make_test_data_file(file_name, ticker_str)
 
-    df = df[["시가", "종가", "등락률"]]
-    df.to_csv(file_name)
-    print("New file is made. File name : ", file_name)
 
 tmp_ticker = '105560'
 get_stock_price_data(tmp_ticker)
