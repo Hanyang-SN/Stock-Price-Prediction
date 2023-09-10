2e98ca600f196e7c296906b3c277ec19de9458c3
1092soobin2 <1092soobin2@gmail.com>
2023-09-10 12:39:02 +0900

    번역기 (preprocessor.py) 주석 추가

--- text_data_application/preprocessor.py
+++ text_data_application/preprocessor.py
@@ -6,11 +6,15 @@ translator = Translator()
 
 def preprocesser(sentences: pd.DataFrame):
     try:
+        # 번역
         translated_sentences = []
         for sentence in sentences['title-ko']:
             translated_sentences.append(translator.translate(str(sentence), 'en', 'ko').text)
 
+        # 데이터 프레임에 열 추가
         sentences["title-en"] = translated_sentences
     except TypeError as e:
+        print("----------------------------------")
         print("Exception Occured:", e)
+        print("----------------------------------")
     return sentences
